fe805c2288f69b67580f8bf6382b36a972815921
Jason Rueckert <phlacin@gmail.com>
2019-06-08 23:57:00 -0700

    Updated folder structure: ClockViewController and WakeScreenViewController

--- Waker/Resources/Emitter.swift
+++ Waker/Resources/Emitter.swift
@@ -37,11 +37,11 @@ class Emitter {
         return cells
     }
 }
-
+// MARK: Extension Float
 public extension Float {
     
     /// Returns a random floating point number between 0.0 and 1.0, inclusive.
-    public static var random: Float {
+    static var random: Float {
         return Float(arc4random()) / 0xFFFFFFFF
     }
     
@@ -49,7 +49,7 @@ public extension Float {
     ///
     /// - Parameter n:  Interval max
     /// - Returns:      Returns a random float point number between 0 and n max
-    public static func random(min: Float, max: Float) -> Float {
+    static func random(min: Float, max: Float) -> Float {
         return Float.random * (max - min) + min
     }
 }
