ec86f7bc2f8162c6a7bf54de220fa191c4b3596b
Jason Rueckert <phlacin@gmail.com>
2019-06-11 07:02:13 -0700

    testing with rounded boxes

--- Waker/ViewControllers/WakeScreenViewController.swift
+++ Waker/ViewControllers/WakeScreenViewController.swift
@@ -13,6 +13,44 @@ class WakeScreenViewController: UIViewController, HolderViewDelegate {
     
     var holderView = HolderView(frame: .zero)
     
+    
+    
+    @IBOutlet weak var barsView: UIImageView!
+    
+    // Circles
+    let shapeLayer1 = CAShapeLayer()
+    var currentStage = 1
+    
+    
+    // Columns
+    var imageView: UIImageView!
+    
+    @IBOutlet weak var exitButton: UIButton!
+    
+    // UserDefault Info
+    let defaults = UserDefaults.standard
+    let preWakeDurationKey = "preWakeDuration"
+    let wakeDurationKey = "wakeDuration"
+    let flipModeSwitchStateKey = "flipModeSwitchState"
+    
+    var preWakeDurationValuePiece : Double = 0
+    var wakeDurationValueInterval : TimeInterval = 0
+    
+    var hasBeenClicked = false
+    
+    //MARK: - View Loading Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        if let preWakeDurationValue = defaults.object(forKey: preWakeDurationKey) as? TimeInterval {
+            preWakeDurationValuePiece = preWakeDurationValue
+        }
+        if let wakeDurationValue = defaults.object(forKey: wakeDurationKey) as? TimeInterval {
+            wakeDurationValueInterval = wakeDurationValue
+        }
+    }
+    
     func addHolderView() {
         let boxSize: CGFloat = 100.0
         holderView.frame = CGRect(x: view.bounds.width / 2 - boxSize / 2,
@@ -29,11 +67,11 @@ class WakeScreenViewController: UIViewController, HolderViewDelegate {
         holderView.removeFromSuperview()
         view.backgroundColor = Colors.blue
         
-        var label: UILabel = UILabel(frame: view.frame)
+        let label: UILabel = UILabel(frame: view.frame)
         label.textColor = Colors.white
         label.font = UIFont(name: "HelveticaNeue-Thin", size: 170.0)
         label.textAlignment = .center
-        label.text = "S"
+        label.text = "Wake Up!"
         label.transform = label.transform.scaledBy(x: 0.25, y: 0.25)
         view.addSubview(label)
         
@@ -44,144 +82,90 @@ class WakeScreenViewController: UIViewController, HolderViewDelegate {
         }
     }
     
-    @IBOutlet weak var barsView: UIImageView!
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if defaults.bool(forKey: flipModeSwitchStateKey) {
+            self.view.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        }
+        addHolderView()
+    }
     
-    // Circles
-    let shapeLayer1 = CAShapeLayer()
-    var currentStage = 1
-    
-    
-    // Columns
-    var imageView: UIImageView!
-    
-    @IBOutlet weak var greenWakeImage: UIImageView!
-    @IBOutlet weak var exitButton: UIButton!
-    @IBOutlet weak var presentImage: UIButton!
-    @IBOutlet weak var presentPopup: UIView!
-    
-    @IBOutlet weak var characterButton: UIButton!
-    
-    // UserDefault Info
-    let defaults = UserDefaults.standard
-    let preWakeDurationKey = "preWakeDuration"
-    let wakeDurationKey = "wakeDuration"
-    let flipModeSwitchStateKey = "flipModeSwitchState"
-    
-    var preWakeDurationValuePiece : Double = 0
-    var wakeDurationValueInterval : TimeInterval = 0
-    
-    var hasBeenClicked = false
-    // Character
-    var forCousteau: Bool = true
-    //    var forCousteau: Bool = false
-    var characterPicked = false
-    @IBOutlet weak var characterImage: UIImageView!
-    
-    //    private var rainSound: Sound?
-    
-    //MARK: - View Loading Methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        greenWakeImage.isHidden = true
-        exitButton.isHidden = true
-        //        presentImage.isHidden = true
-        presentPopup.alpha = 0
+    fileprivate func bar(at position: Int) {
+        let width = view.bounds.width
+        imageView = UIImageView(frame: CGRect(x: CGFloat(width/3) * CGFloat(position), y: 0, width: width/3, height: view.bounds.height))
+        imageView.backgroundColor = UIColor.red
         
-        if let preWakeDurationValue = defaults.object(forKey: preWakeDurationKey) as? TimeInterval {
-            preWakeDurationValuePiece = preWakeDurationValue
+        view.addSubview(imageView)
+        imageView.setAnchorPoint(CGPoint(x: 0, y: 0))
+        if position == 0 {
+            UIView.animate(withDuration: 3, delay: 1, options: .curveLinear, animations: {
+                self.imageView.transform = CGAffineTransform(scaleX: 1, y: 0.001)
+            }) { (success) in
+                self.imageView.removeFromSuperview()
+            }
         }
-        if let wakeDurationValue = defaults.object(forKey: wakeDurationKey) as? TimeInterval {
-            wakeDurationValueInterval = wakeDurationValue
+    }
+    
+    func showStage() {
+        bar(at: 0)
+        //        bar(at: 1)
+        //        bar(at: 2)
+    }
+    
+    
+    func WakeUpTime() {
+        print("Wake Up!")
+//        UIView.animate(withDuration: 0.001, delay: 5, animations: {
+////            self.greenWakeImage.isHidden = false
+////            self.exitButton.isHidden = false
+//        }) { (true) in
+//            if self.forCousteau {
+//                //                self.presentImage.isHidden = false
+//                self.presentPopup.transform = CGAffineTransform(scaleX: 0.3, y: 2)
+//                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
+//                    self.presentPopup.transform = .identity
+//                }, completion: { (success) in
+//
+//                })
+//                self.presentPopup.alpha = 1
+//                //                self.randomlyPickCharacter()
+//
+//            }
+//            Timer.scheduledTimer(withTimeInterval: self.wakeDurationValueInterval, repeats: false, block: { (Timer) in
+//                self.defaults.set(false, forKey: "hasFiredKey")
+//                self.performSegueToReturnBack()
+//                self.hasBeenClicked = false
+//            })
+//        }
+    }
+    
+    @IBAction func exitPressed(_ sender: UIButton) {
+        dismiss(animated: false)
+    }
+    
+    @IBAction func giftBoxButton(_ sender: Any) {
+        if !hasBeenClicked {
+            hasBeenClicked = true
         }
+    }
+    // TODO: - Change this from a UIImage to a UIButton and make it clickable
+    @IBAction func characterButtonPressed(_ sender: Any) {
         
     }
-
-
-
-override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    if defaults.bool(forKey: flipModeSwitchStateKey) {
-        self.view.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-    }
-    addHolderView()
-}
-
-fileprivate func bar(at position: Int) {
-    let width = view.bounds.width
-    imageView = UIImageView(frame: CGRect(x: CGFloat(width/3) * CGFloat(position), y: 0, width: width/3, height: view.bounds.height))
-    imageView.backgroundColor = UIColor.red
     
-    view.addSubview(imageView)
-    imageView.setAnchorPoint(CGPoint(x: 0, y: 0))
-    if position == 0 {
-        UIView.animate(withDuration: 3, delay: 1, options: .curveLinear, animations: {
-            self.imageView.transform = CGAffineTransform(scaleX: 1, y: 0.001)
-        }) { (success) in
-            self.imageView.removeFromSuperview()
-        }
-    }
-}
-
-func showStage() {
-    bar(at: 0)
-    //        bar(at: 1)
-    //        bar(at: 2)
-}
-
-
-func WakeUpTime() {
-    UIView.animate(withDuration: 0.001, delay: 5, animations: {
-        self.greenWakeImage.isHidden = false
-        self.exitButton.isHidden = false
-    }) { (true) in
-        if self.forCousteau {
-            //                self.presentImage.isHidden = false
-            self.presentPopup.transform = CGAffineTransform(scaleX: 0.3, y: 2)
-            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
-                self.presentPopup.transform = .identity
-            }, completion: { (success) in
-                
-            })
-            self.presentPopup.alpha = 1
-            //                self.randomlyPickCharacter()
-            
-        }
-        Timer.scheduledTimer(withTimeInterval: self.wakeDurationValueInterval, repeats: false, block: { (Timer) in
-            self.defaults.set(false, forKey: "hasFiredKey")
-            self.performSegueToReturnBack()
-            self.hasBeenClicked = false
-        })
-    }
-}
-
-@IBAction func exitPressed(_ sender: UIButton) {
-    dismiss(animated: false)
-}
-
-@IBAction func giftBoxButton(_ sender: Any) {
-    presentPopup.alpha = 0
-    if !hasBeenClicked {
-        hasBeenClicked = true
-    }
-}
-// TODO: - Change this from a UIImage to a UIButton and make it clickable
-@IBAction func characterButtonPressed(_ sender: Any) {
-    
-}
-
-func randomlyPickCharacter() {
-    characterPicked = false
-    let numberOfImages: UInt32 = 29
-    let random = arc4random_uniform(numberOfImages)
-    let imageName = "image_\(random)"
-    if characterPicked == false {
-        characterImage.image = UIImage(named: imageName)
-        let myImage = characterImage.image
-        characterButton.setImage(myImage, for: UIControlState.normal)
-        
-        characterPicked = true
-    }
-}
+//    func randomlyPickCharacter() {
+//        characterPicked = false
+//        let numberOfImages: UInt32 = 29
+//        let random = arc4random_uniform(numberOfImages)
+//        let imageName = "image_\(random)"
+//        if characterPicked == false {
+//            characterImage.image = UIImage(named: imageName)
+//            let myImage = characterImage.image
+//            characterButton.setImage(myImage, for: UIControlState.normal)
+//            
+//            characterPicked = true
+//        }
+//    }
 }
 
 extension UIViewController {
--- Waker/ViewModels/RectangleLayer.swift
+++ Waker/ViewModels/RectangleLayer.swift
@@ -12,16 +12,18 @@ class RectangleLayer: CAShapeLayer {
     override init() {
         super.init()
         fillColor = Colors.clear.cgColor
-        lineWidth = 5.0
-        path = rectanglePathFull.cgPath
+        lineWidth = 10.0
+        path = roundedRectanglePath.cgPath
     }
     
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    var roundedRectanglePath = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 160, height: 160), cornerRadius: 50)
+    
     var rectanglePathFull: UIBezierPath {
-        var rectanglePath = UIBezierPath()
+        let rectanglePath = UIBezierPath()
         rectanglePath.move(to: CGPoint(x: 0.0, y: 100.0))
         rectanglePath.addLine(to: CGPoint(x: 0.0, y: -lineWidth))
         rectanglePath.addLine(to: CGPoint(x: 100.0, y: -lineWidth))
@@ -33,7 +35,7 @@ class RectangleLayer: CAShapeLayer {
     
     func animateStrokeWithColor(color: UIColor, duration: Double) {
         strokeColor = color.cgColor
-        var strokeAnimation: CABasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        let strokeAnimation: CABasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         strokeAnimation.fromValue = 0.0
         strokeAnimation.toValue = 1.0
         strokeAnimation.duration = duration
