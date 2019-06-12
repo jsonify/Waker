cc249ed3d7392ba573c0853a11e42fa56f6ca3d3
Jason Rueckert <phlacin@gmail.com>
2019-06-11 23:45:37 -0700

    Cleaning up unneeded code

--- Waker/ViewControllers/SettingsViewController/SettingsViewController.swift
+++ Waker/ViewControllers/SettingsViewController/SettingsViewController.swift
@@ -25,14 +25,14 @@ class SettingsViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        UIApplication.shared.statusBarStyle = .lightContent
+//        UIApplication.shared.statusBarStyle = .lightContent
 
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
 
-        UIApplication.shared.statusBarStyle = .default
+//        UIApplication.shared.statusBarStyle = .default
     }
 
     
